bb51ef03ebb6ca31c3bf8cd863b24ca12a06e7fd
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2021-07-21 00:36:38 +0900

    change firebase json
    지노에서 건형으로 테스트용으로 변경

--- app/src/main/java/com/example/swhackerton/MainActivity.kt
+++ app/src/main/java/com/example/swhackerton/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.swhackerton
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSignOutBtn()
     }
 
     override fun onStart() {
         super.onStart()
 
         if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    fun initSignOutBtn() {
+        var signOut = findViewById<Button>(R.id.signOut)
+        signOut.setOnClickListener {
+            auth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
