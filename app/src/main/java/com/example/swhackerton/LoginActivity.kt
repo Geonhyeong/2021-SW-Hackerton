97cb0da402e0495a5d53ebacfb32b99876b84e4b
fbwlsgh93 <42794341+fbwlsgh93@users.noreply.github.com>
2021-07-20 10:07:45 +0900

    0720_1007_ui
    회원가입 화면 변경

--- app/src/main/java/com/example/swhackerton/LoginActivity.kt
+++ app/src/main/java/com/example/swhackerton/LoginActivity.kt
@@ -54,25 +54,7 @@ class LoginActivity : AppCompatActivity() {
 
         signUpButton.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
-//            val email = getInputEmail()
-//            val password = getInputPassword()
-//
-//            auth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this) { task ->
-//                    if (task.isSuccessful) {
-//                        Toast.makeText(
-//                            this,
-//                            "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    } else {
-//                        Toast.makeText(
-//                            this,
-//                            "이미 가입한 이메일이거나, 회원가입에 실패했습니다.",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                }
+
         }
     }
 
@@ -80,22 +62,18 @@ class LoginActivity : AppCompatActivity() {
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
         val loginButton = findViewById<Button>(R.id.loginButton)
-        val signUpButton = findViewById<Button>(R.id.signUpButton)
 
+        loginButton.isEnabled = false
 
         emailEditText.addTextChangedListener {
             val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
             loginButton.isEnabled = enable
-            signUpButton.isEnabled = enable
         }
 
         passwordEditText.addTextChangedListener {
             val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
             loginButton.isEnabled = enable
-            signUpButton.isEnabled = enable
         }
-
-
     }
 
     private fun getInputEmail(): String {
--- app/src/main/java/com/example/swhackerton/SignUpActivity.kt
+++ app/src/main/java/com/example/swhackerton/SignUpActivity.kt
@@ -1,11 +1,17 @@
 package com.example.swhackerton
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private val signUpButton: AppCompatButton by lazy {
         findViewById(R.id.signUpButton)
@@ -20,7 +26,34 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun initSignUpButton() {
         signUpButton.setOnClickListener {
-            finish()
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원가입에 성공했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "이미 가입한 이메일이거나, 회원가입에 실패했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
+    }
+
+
+    private fun getInputEmail(): String {
+        return findViewById<EditText>(R.id.emailEditText).text.toString()
+    }
+
+    private fun getInputPassword(): String {
+        return findViewById<EditText>(R.id.passwordEditText).text.toString()
     }
 }
