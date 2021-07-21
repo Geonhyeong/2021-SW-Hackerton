46562d040bc874f39b0656e4171c2929fa995da5
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2021-07-21 13:58:11 +0900

    Recyclerview 추가 및 db 연동
    방 레이아웃을 추가하고 DB에서 이벤트를 받아 동적으로 리스트가 추가되도록 함

--- app/src/main/java/com/example/swhackerton/MainActivity.kt
+++ app/src/main/java/com/example/swhackerton/MainActivity.kt
@@ -4,41 +4,107 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.swhackerton.adapter.RoomAdapter
 import com.example.swhackerton.databinding.ActivityMainBinding
+import com.example.swhackerton.model.Room
+import com.example.swhackerton.model.User
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.card.MaterialCardView
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private val auth = FirebaseAuth.getInstance()
     private val currentRoomDB = Firebase.database.reference.child("Rooms")
+    private val currentUserDB = Firebase.database.reference.child("Users")
     lateinit var binding : ActivityMainBinding
+    private lateinit var adapter: RoomAdapter
+    var roomArray : MutableList<Room> = arrayListOf()
+    private lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        roomArray.clear()
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                if (snapshot.key?.isNotEmpty() == true) {
+                    getRoomByKey(snapshot.key.orEmpty())
+                }
+            }
 
-        initJoinRoom()
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        currentRoomDB.addChildEventListener(childEventListener)
+
+        
+
+        //initUserData()
+        initRoomRecyclerView()
+        //initJoinRoom()
         initCreateRoomBtn()
         initSignOutBtn()
     }
 
+    fun initUserData() {
+        var userId = auth.currentUser?.uid.toString()
+        currentUserDB.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                user = User(
+                    snapshot.child("name").value.toString(),
+                    snapshot.child("nickName").value.toString(),
+                    snapshot.child("sex").value.toString(),
+                    snapshot.child("stuNum").value.toString(),
+                    userId)
+                println("USER : " + user)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        println("USER : " + user)
+    }
+
+    fun initRoomRecyclerView() {
+        adapter = RoomAdapter()
+        binding.roomRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.roomRecyclerView.adapter = adapter
+    }
+
+
     fun initJoinRoom() {
-        var joinRoom = findViewById<MaterialCardView>(R.id.card_view01)
+        var joinRoom = findViewById<MaterialCardView>(R.id.card_view)
 
         joinRoom.setOnClickListener {
             startActivity(Intent(this, VoiceActivity::class.java))
@@ -46,7 +112,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initCreateRoomBtn() {
-        var extendedfab = findViewById<ExtendedFloatingActionButton>(R.id.extended_fab)
+        var extendedfab = findViewById<AppCompatButton>(R.id.extended_fab)
 
         extendedfab.setOnClickListener {
             val dialog = BottomSheetDialog(this)
@@ -75,11 +141,12 @@ class MainActivity : AppCompatActivity() {
             val btnClose = view.findViewById<Button>(R.id.idBtnDismiss)
             btnClose.setOnClickListener {
                 var Room = mutableMapOf<String, Any>()
+
                 Room["title"] = title
-                Room["Owner"] = auth.currentUser?.uid.toString()
+                Room["OwnerNickname"] = auth.currentUser?.email.toString()
+                Room["OwnerUid"] = auth.currentUser?.uid.toString()
 
                 currentRoomDB.child(title).updateChildren(Room)
-
                 dialog.dismiss()
             }
             dialog.setCancelable(false)
@@ -94,5 +161,18 @@ class MainActivity : AppCompatActivity() {
             auth.signOut()
             finish()
         }
+    }
+
+    private fun getRoomByKey(roomTitle: String) {
+        currentRoomDB.child(roomTitle).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                roomArray.add(Room(snapshot.child("title").value.toString(), snapshot.child("OwnerNickname").value.toString(),snapshot.child("OwnerUid").value.toString()))
+                adapter.submitList(roomArray)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/swhackerton/model/User.kt
@@ -0,0 +1,3 @@
+package com.example.swhackerton.model
+
+data class User(val name: String, val nickName: String, val sex: String, val stuNum: String, val userId: String)
